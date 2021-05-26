925157ffe437df1a4aa121a24d4727dc7e87ec3d
JiwoonKim <jiwoon.kim@wrl.onl>
2021-05-26 17:37:56 +0900

    add HorizontalCamera shader graph

---
+++ Assets/Scripts/Test/ObjectFade.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFade : MonoBehaviour
+{
+    public Camera camera;
+    public float fadeTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera.GetComponent<FadeCamera>().FadeIn(fadeTime);
+    }
+
+ 
+}
